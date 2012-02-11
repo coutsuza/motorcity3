f6b44fa2d2713d1e841b99ef28d3cba99f1f7682
Suzanne <coutsuza@gmail.com>
2012-02-11 09:21:08 -0500

    Adding all existing file to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motorcity3::Application.config.secret_token = '3d65f4b946f9ae0a2bde3f26bd9059069a3ef27278441aa77f03154be75cd9b495d93c25cb886bcd179e4d9e27584fbd171d79ea82105dd7f20e7c6fbcfddc49'
